dcae3119b635af6f3e16f9d763396695450ce801
zj24 <zj24@mails.tsinghua.edu.cn>
2025-08-23 00:23:22 +0800

    use fmt to standard the format

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
+use colored::Colorize;
 use std::char;
 use std::collections::HashMap;
 use std::io::{self, Write};
-use colored::Colorize;
 mod builtin_words;
-use rand::Rng;
 use builtin_words::ACCEPTABLE;
 use builtin_words::FINAL;
+use rand::Rng;
 
-fn pr(c:char){
-    match &c{
-        'R' => print!("{}","R".bold().red()),
-        'Y' => print!("{}","Y".bold().yellow()),
-        'G' => print!("{}","G".bold().green()),
-        'X' => print!("{}","X".bold()),
+fn pr(c: char) {
+    match &c {
+        'R' => print!("{}", "R".bold().red()),
+        'Y' => print!("{}", "Y".bold().yellow()),
+        'G' => print!("{}", "G".bold().green()),
+        'X' => print!("{}", "X".bold()),
         _ => print!("invaild"),
     }
 }
 
 struct GameHistory {
-    s_status_history: Vec<char>,    
-    char_status_history: Vec<char>, 
-    guesses: String,                 
+    s_status_history: Vec<char>,
+    char_status_history: Vec<char>,
+    guesses: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let is_tty = atty::is(atty::Stream::Stdout);
-    let mut game_record:Vec<GameHistory>=Vec::new();
+    let mut game_record: Vec<GameHistory> = Vec::new();
     if is_tty {
         println!(
             "I am in a tty. Please print {}!",
@@ -39,207 +39,246 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         io::stdout().flush().unwrap();
         print!("Input the answer word:");
         io::stdout().flush().unwrap();
-        let mut answer_word=String::new();
+        let mut answer_word = String::new();
         io::stdin().read_line(&mut answer_word)?;
         // let rand_index = rand::thread_rng().gen_range(0..FINAL.len());
         // let answer_word = FINAL[rand_index];
         io::stdout().flush().unwrap();
 
-        let mut chracter_status:Vec<char>=['X';26].to_vec();
-        let answer_word_vector:Vec<char> = answer_word.chars().collect();
-        let mut appearance:HashMap<char,i32> = HashMap::new();
-        for c in answer_word.chars(){
-                let pos = appearance.entry(c).or_insert(0);
-                *pos+=1;
-            }
-        
-        let mut guess=String::new();
-        let mut turn =1;
-        let mut flag:bool = false;
-        while turn<6{
-            println!("You have {} chance left,Input you guess:",6-turn);
+        let mut chracter_status: Vec<char> = ['X'; 26].to_vec();
+        let answer_word_vector: Vec<char> = answer_word.chars().collect();
+        let mut appearance: HashMap<char, i32> = HashMap::new();
+        for c in answer_word.chars() {
+            let pos = appearance.entry(c).or_insert(0);
+            *pos += 1;
+        }
+
+        let mut guess = String::new();
+        let mut turn = 1;
+        let mut flag: bool = false;
+        while turn < 6 {
+            println!("You have {} chance left,Input you guess:", 6 - turn);
             io::stdin().read_line(&mut guess)?;
             guess = guess.to_lowercase();
 
-            let mut guess_word_vector:Vec<char> = guess.chars().collect();
+            let mut guess_word_vector: Vec<char> = guess.chars().collect();
             let mut guess_appearance = appearance.clone();
-            let mut s_status:Vec<char>=['R';5].to_vec();
-            let mut input_flag:bool = true;
+            let mut s_status: Vec<char> = ['R'; 5].to_vec();
+            let mut input_flag: bool = true;
 
-            
-            if !(ACCEPTABLE.contains(&guess.trim())){
+            if !(ACCEPTABLE.contains(&guess.trim())) {
                 println!("INVALID");
                 input_flag = false;
-            }
-            else if guess.len()!=6 {
+            } else if guess.len() != 6 {
                 println!("INVALID");
                 input_flag = false;
-            }
-            else{
-            for i in 0..5{
-                if !((guess_word_vector[i]>='A'&&guess_word_vector[i]<='Z')||(guess_word_vector[i]>='a'&&guess_word_vector[i]<='z')) {
-                    println!("INVALID");
-                    input_flag = false;
-                    break;
-                 }
-             }}
-            if input_flag {
-            for i in 0..5{
-                if guess_word_vector[i]==answer_word_vector[i] {
-                    s_status[i]='G';
-                    *guess_appearance.entry(answer_word_vector[i]).or_insert(0)-=1;
+            } else {
+                for i in 0..5 {
+                    if !((guess_word_vector[i] >= 'A' && guess_word_vector[i] <= 'Z')
+                        || (guess_word_vector[i] >= 'a' && guess_word_vector[i] <= 'z'))
+                    {
+                        println!("INVALID");
+                        input_flag = false;
+                        break;
+                    }
                 }
             }
-            for i in 0..5{
-                if guess_word_vector[i]!=answer_word_vector[i] {
-                    if let Some(&x) = guess_appearance.get(&guess_word_vector[i]){
-                        if x>0 {
-                            s_status[i]='Y';
-                            *guess_appearance.entry(guess_word_vector[i]).or_insert(0)-=1;
+            if input_flag {
+                for i in 0..5 {
+                    if guess_word_vector[i] == answer_word_vector[i] {
+                        s_status[i] = 'G';
+                        *guess_appearance.entry(answer_word_vector[i]).or_insert(0) -= 1;
+                    }
+                }
+                for i in 0..5 {
+                    if guess_word_vector[i] != answer_word_vector[i] {
+                        if let Some(&x) = guess_appearance.get(&guess_word_vector[i]) {
+                            if x > 0 {
+                                s_status[i] = 'Y';
+                                *guess_appearance.entry(guess_word_vector[i]).or_insert(0) -= 1;
+                            }
                         }
                     }
-                }
-                match s_status[i]{
-                    
-                    'G' => chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='G',
-                    'Y' if  chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='G' => chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='Y',
-                    'R' if (chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='G'&&chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='Y')=>{
-                        chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='R';
+                    match s_status[i] {
+                        'G' => {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'G'
+                        }
+                        'Y' if chracter_status[((guess_word_vector[i] as u8) - b'a') as usize]
+                            != 'G' =>
+                        {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'Y'
+                        }
+                        'R' if (chracter_status
+                            [((guess_word_vector[i] as u8) - b'a') as usize]
+                            != 'G'
+                            && chracter_status[((guess_word_vector[i] as u8) - b'a') as usize]
+                                != 'Y') =>
+                        {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'R';
+                        }
+                        _ => continue,
                     }
-                    _ => continue,
+                }
+
+                game_record.push(GameHistory {
+                    s_status_history: s_status.clone(),
+                    char_status_history: chracter_status.clone(),
+                    guesses: guess.clone(),
+                });
+
+                for history_iter in game_record.iter() {
+                    flag = true;
+                    for iter in history_iter.s_status_history.iter() {
+                        pr(*iter);
+                        if *iter != 'G' {
+                            flag = false;
+                        }
+                    }
+                    print!("{}", ' ');
+                    for iter in history_iter.char_status_history.iter() {
+                        pr(*iter);
+                    }
+                    println!("");
                 }
             }
-            
-            game_record.push(GameHistory { s_status_history: s_status.clone(), char_status_history: chracter_status.clone(), guesses: guess.clone()});
 
-              
-            for history_iter in game_record.iter(){
-                flag = true;
-                for iter in history_iter.s_status_history.iter(){
-                    pr(*iter);
-                    if *iter!='G' { flag = false;}
+            if flag {
+                break;
+            }
+            if input_flag {
+                turn += 1;
+            }
+            s_status.clear();
+            guess.clear();
+            guess_appearance.clear();
+            guess_word_vector.clear();
+        }
+
+        println!("Guess turns:{}", turn);
+        if !flag {
+            println!("Answer:{}", answer_word.to_uppercase());
+        }
+
+    // // example: print arguments
+    // print!("Command line arguments: ");
+    // for arg in std::env::args() {
+    //     print!("{} ", arg);
+    // }
+    // // TODO: parse the arguments in `args`
+    } else {
+        // let rand_index = rand::thread_rng().gen_range(0..FINAL.len());
+        // let answer_word = FINAL[rand_index];
+        // println!("{}",answer_word)
+        let mut answer_word = String::new();
+        io::stdin().read_line(&mut answer_word)?;
+        // let rand_index = rand::thread_rng().gen_range(0..FINAL.len());
+        // let answer_word = FINAL[rand_index];
+        io::stdout().flush().unwrap();
+
+        let mut chracter_status: Vec<char> = ['X'; 26].to_vec();
+        let answer_word_vector: Vec<char> = answer_word.chars().collect();
+        let mut appearance: HashMap<char, i32> = HashMap::new();
+        for c in answer_word.chars() {
+            let pos = appearance.entry(c).or_insert(0);
+            *pos += 1;
+        }
+
+        let mut guess = String::new();
+        let mut turn = 1;
+        let mut flag: bool = false;
+        while turn <= 6 {
+            io::stdin().read_line(&mut guess)?;
+            guess = guess.to_lowercase();
+
+            let mut guess_word_vector: Vec<char> = guess.chars().collect();
+            let mut guess_appearance = appearance.clone();
+            let mut s_status: Vec<char> = ['R'; 5].to_vec();
+            let mut input_flag: bool = true;
+
+            if !(ACCEPTABLE.contains(&guess.trim())) {
+                println!("INVALID");
+                input_flag = false;
+            } else if guess.len() != 6 {
+                println!("INVALID");
+                input_flag = false;
+            } else {
+                for i in 0..5 {
+                    if !((guess_word_vector[i] >= 'A' && guess_word_vector[i] <= 'Z')
+                        || (guess_word_vector[i] >= 'a' && guess_word_vector[i] <= 'z'))
+                    {
+                        println!("INVALID");
+                        input_flag = false;
+                        break;
+                    }
                 }
-                print!("{}",' ');
-                for iter in history_iter.char_status_history.iter(){
+            }
+
+            if input_flag {
+                for i in 0..5 {
+                    if guess_word_vector[i] == answer_word_vector[i] {
+                        s_status[i] = 'G';
+                        *guess_appearance.entry(answer_word_vector[i]).or_insert(0) -= 1;
+                    }
+                }
+                for i in 0..5 {
+                    if guess_word_vector[i] != answer_word_vector[i] {
+                        if let Some(&x) = guess_appearance.get(&guess_word_vector[i]) {
+                            if x > 0 {
+                                s_status[i] = 'Y';
+                                *guess_appearance.entry(guess_word_vector[i]).or_insert(0) -= 1;
+                            }
+                        }
+                    }
+                    match s_status[i] {
+                        'G' => {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'G'
+                        }
+                        'Y' if chracter_status[((guess_word_vector[i] as u8) - b'a') as usize]
+                            != 'G' =>
+                        {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'Y'
+                        }
+                        'R' if (chracter_status
+                            [((guess_word_vector[i] as u8) - b'a') as usize]
+                            != 'G'
+                            && chracter_status[((guess_word_vector[i] as u8) - b'a') as usize]
+                                != 'Y') =>
+                        {
+                            chracter_status[((guess_word_vector[i] as u8) - b'a') as usize] = 'R';
+                        }
+                        _ => continue,
+                    }
+                }
+                flag = true;
+                for iter in s_status.iter() {
+                    pr(*iter);
+                    if *iter != 'G' {
+                        flag = false;
+                    }
+                }
+                print!("{}", ' ');
+                for iter in chracter_status.iter() {
                     pr(*iter);
                 }
                 println!("");
-                }
-            } 
-
-            if flag {break;}
-            if input_flag{turn+=1;}
-            s_status.clear();
-            guess.clear();
-            guess_appearance.clear();
-            guess_word_vector.clear();
-        }
-
-        println!("Guess turns:{}",turn);
-        if !flag { println!("Answer:{}",answer_word.to_uppercase());} 
-
-        // // example: print arguments
-        // print!("Command line arguments: ");
-        // for arg in std::env::args() {
-        //     print!("{} ", arg);
-        // }
-        // // TODO: parse the arguments in `args`
-        }
-    else{
-        // let rand_index = rand::thread_rng().gen_range(0..FINAL.len());
-        // let answer_word = FINAL[rand_index];
-        // println!("{}",answer_word) 
-        let mut answer_word=String::new();
-        io::stdin().read_line(&mut answer_word)?;
-        // let rand_index = rand::thread_rng().gen_range(0..FINAL.len());
-        // let answer_word = FINAL[rand_index];
-        io::stdout().flush().unwrap();
-
-        let mut chracter_status:Vec<char>=['X';26].to_vec();
-        let answer_word_vector:Vec<char> = answer_word.chars().collect();
-        let mut appearance:HashMap<char,i32> = HashMap::new();
-        for c in answer_word.chars(){
-                let pos = appearance.entry(c).or_insert(0);
-                *pos+=1;
             }
-        
-        let mut guess=String::new();
-        let mut turn =1;
-        let mut flag:bool = false;
-        while turn<=6{
-            io::stdin().read_line(&mut guess)?;
-            guess = guess.to_lowercase();
-
-            let mut guess_word_vector:Vec<char> = guess.chars().collect();
-            let mut guess_appearance = appearance.clone();
-            let mut s_status:Vec<char>=['R';5].to_vec();
-            let mut input_flag:bool = true;
-
-            if !(ACCEPTABLE.contains(&guess.trim())){
-                println!("INVALID");
-                input_flag = false;
+            if flag {
+                break;
             }
-            else if guess.len()!=6 {
-                println!("INVALID");
-                input_flag = false;
-            }
-            else{
-            for i in 0..5{
-                if !((guess_word_vector[i]>='A'&&guess_word_vector[i]<='Z')||(guess_word_vector[i]>='a'&&guess_word_vector[i]<='z')) {
-                    println!("INVALID");
-                    input_flag = false;
-                    break;
-                 }
-             }}
-
             if input_flag {
-            for i in 0..5{
-                if guess_word_vector[i]==answer_word_vector[i] {
-                    s_status[i]='G';
-                    *guess_appearance.entry(answer_word_vector[i]).or_insert(0)-=1;
-                }
+                turn += 1;
             }
-            for i in 0..5{
-                if guess_word_vector[i]!=answer_word_vector[i] {
-                    if let Some(&x) = guess_appearance.get(&guess_word_vector[i]){
-                        if x>0 {
-                            s_status[i]='Y';
-                            *guess_appearance.entry(guess_word_vector[i]).or_insert(0)-=1;
-                        }
-                    }
-                }
-                match s_status[i]{
-                    
-                    'G' => chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='G',
-                    'Y' if  chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='G' => chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='Y',
-                    'R' if (chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='G'&&chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]!='Y')=>{
-                        chracter_status[((guess_word_vector[i] as u8)-b'a' )as usize]='R';
-                    }
-                    _ => continue,
-                }
-            }
-            flag = true;
-            for iter in s_status.iter(){
-                pr(*iter);
-                if *iter!='G' { flag = false;}
-            }
-            print!("{}",' ');
-            for iter in chracter_status.iter(){
-                pr(*iter);
-            }
-            println!("");
-        }   
-            if flag {break;}
-            if input_flag{turn+=1;}
             s_status.clear();
             guess.clear();
             guess_appearance.clear();
             guess_word_vector.clear();
         }
 
-        if flag{println!("CORRECT {}",turn);}
-        else { println!("FAILED {}",answer_word.to_uppercase());} 
+        if flag {
+            println!("CORRECT {}", turn);
+        } else {
+            println!("FAILED {}", answer_word.to_uppercase());
+        }
     }
     Ok(())
 }
-
